13a7eec1465a72e4c7cf34a9ffaf9017007d72b1
silviapopa1 <silviapopa1@gmail.com>
2014-10-25 11:14:36 -0400

    Intégration de la classe Statistique
    Ajout de la classe modèle et des tests

--- modeles/Statistique.class.php
+++ modeles/Statistique.class.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
     class Statistique extends Modele {
         
         //toutes les fonctions retournent un résultat en fonction de la commission ou de l'école à laquelle ils appartiennent.
@@ -7,56 +7,355 @@
             
         }
         
-        public function rechercherNbVisitesParMois(){
-            $oUtilisateur = new Utilisateur();
-            $user_id = $oUtilisateur -> getUserId();
-            
-            
+        public function rechercherNbVisitesParMois($user_id){
+			
+			TypeException::estInteger($user_id);
+			
+            $annee_debut = "";
+			$annee_fin = "";
+			
+			if(date("n") <= 6){
+				$annee_debut = date("Y")-1;
+				$annee_fin = date("Y");
+			}
+			else{
+				$annee_debut = date("Y");
+				$annee_fin = date("Y")+1;
+			}
+			
             $oConnexion = new MySqliLib();
-            $oResultat = $oConnexion->executer("SELECT COUNT(*) FROM activite_login al WHERE al.utilisateur_ID = '".$user_id."' AND evenement = 'login' AND ((YEAR(timestamp) = '".$annee_debut."' AND MONTH(timestamp) >= '8') OR (YEAR(timestamp) = '".$annee_fin."' AND MONTH(timestamp) <= '6')) GROUP BY MONTH(timestamp) ORDER BY YEAR(timestamp) ASC, MONTH(timestamp) ASC");
-            $aNombreVisites = $oResultat->recupererTableau($oResultat);
+            $oResultat = $oConnexion->executer("SELECT COUNT(*) FROM activite_login al 
+												WHERE al.utilisateur_ID = '".$user_id."' AND evenement = 'login'
+												AND ((YEAR(timestamp) = '".$annee_debut."' AND MONTH(timestamp) >= '8') OR (YEAR(timestamp) = '".$annee_fin."' AND MONTH(timestamp) <= '6')) 
+												GROUP BY MONTH(timestamp)
+												ORDER BY YEAR(timestamp) ASC, MONTH(timestamp) ASC");
+            $aNombreVisites = $oConnexion->recupererTableau($oResultat);
             
             return $aNombreVisites;
         }
         
-        public function rechercherNbTutoratsCrees(){
+        public function rechercherNbTutoratsCrees($commission_ID){
+		
+			TypeException::estInteger($commission_ID);
+		
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT COUNT(*) as NbTutorats
+												FROM contenu c,ecoles e
+												WHERE c.ecole_ID = e.ecole_ID
+												AND e.commission_ID = '".$commission_ID."';");
+			$aNombreTutorats = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTutorats;
+        }
+        
+        public function rechercherNbTutoratsApprouves($commission_ID){
+		
+			TypeException::estInteger($commission_ID);
+		
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT 
+													COUNT(*) as NbTutorats
+												FROM
+													contenu c,ecoles e
+												WHERE
+													c.approuve = 1 AND
+													c.ecole_ID = e.ecole_ID AND
+													e.commission_ID = '".$commission_ID."';
+												");
+			$aNombreTutorats = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTutorats;
         
         }
         
-        public function rechercherNbTutoratsApprouves(){
-        
-        }
-        
-        public function rechercherNbTuteursConversation(){
+        public function rechercherNbTuteursConversation($iEleveId){
             //par mois, avec combien de tuteurs l'élève a communiqué (section messagerie)
+			
+			TypeException::estInteger($iEleveId);
+			
+			$annee_debut = "";
+			$annee_fin = "";
+			
+			if(date("n") <= 6){
+				$annee_debut = date("Y")-1;
+				$annee_fin = date("Y");
+			}
+			else{
+				$annee_debut = date("Y");
+				$annee_fin = date("Y")+1;
+			}
+			
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(DISTINCT cc.tuteur_ID) as NbTuteurConversations,
+													MONTH(mc.timestamp) as mois,
+													YEAR(mc.timestamp) as annee
+												FROM 
+													conversations_chat cc,
+													messages_chat mc
+												WHERE 
+													mc.conversation_ID = cc.conversation_ID AND
+													(
+														(YEAR(mc.timestamp) = '".$annee_debut."' AND MONTH(mc.timestamp) >= '8')
+														OR
+														(YEAR(mc.timestamp) = '".$annee_fin."' AND MONTH(mc.timestamp) <= '6')
+													)
+													AND
+													cc.eleve_ID = '".$iEleveId."'
+												GROUP BY
+													MONTH(mc.timestamp)
+											    ORDER BY 
+													YEAR(mc.timestamp) ASC,
+													MONTH(mc.timestamp) ASC
+												");
+			$aNombreTuteursConversation = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTuteursConversation;
+												
         }
         
         public function rechercherTempsConnecte(){
             //par mois, duree de connexion
         }
         
-        public function rechercherNbElevesConversation(){
+        public function rechercherNbElevesConversation($iTuteurId){
             //par mois, avec combien d'eleves un tuteur a communiqué
+			
+			TypeException::estInteger($iTuteurId);
+			
+			$annee_debut = "";
+			$annee_fin = "";
+			
+			if(date("n") <= 6){
+				$annee_debut = date("Y")-1;
+				$annee_fin = date("Y");
+			}
+			else{
+				$annee_debut = date("Y");
+				$annee_fin = date("Y")+1;
+			}
+			
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(DISTINCT cc.eleve_ID) as NbConversations,
+													MONTH(mc.timestamp) as mois,
+													YEAR(mc.timestamp) as annee
+												FROM 
+													conversations_chat cc,
+													messages_chat mc
+												WHERE 
+													mc.conversation_ID = cc.conversation_ID AND
+													(
+														(YEAR(mc.timestamp) = '".$annee_debut."' AND MONTH(mc.timestamp) >= '8')
+														OR
+														(YEAR(mc.timestamp) = '".$annee_fin."' AND MONTH(mc.timestamp) <= '6')
+													)
+													AND
+													tuteur_ID = '".$iTuteurId."'
+												GROUP BY
+													MONTH(mc.timestamp)
+											    ORDER BY 
+													YEAR(mc.timestamp) ASC,
+													MONTH(mc.timestamp) ASC
+											  ");
+			$aNombreElevesConversation = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreElevesConversation;
         }
         
         public function rechercherFrequenceConnexion(){
             //par mois, combien de temps (jours) entre les connexions
         }
         
-        public function rechercherNbTuteursParEcole(){
-        
+        public function rechercherNbTuteursParEcole($ecole_ID){
+		
+			TypeException::estInteger($ecole_ID);
+		
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT 
+													COUNT(*) as NbTuteurs
+												FROM 
+													utilisateurs u,
+													ecoles_par_utilisateur epu
+												WHERE 
+													u.role = 2 AND 
+													epu.ecole_ID = '".$ecole_ID."'
+												");
+			$aNombreTuteurs = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTuteurs;
+			
         }
         
-        public function rechercherNbProfsParEcole(){
-            
+        public function rechercherNbProfsParEcole($ecole_ID){
+		
+			TypeException::estInteger($ecole_ID);
+		
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(*) as NbProfs
+												FROM 
+													utilisateurs u,
+													ecoles_par_utilisateur epu
+												WHERE 
+													u.role = 3 AND 
+													epu.ecole_ID = '".$ecole_ID."'
+												");
+            $aNombreProfs = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreProfs;
+			
         }
         
-        public function rechercherNbElevesParEcole(){
-            
+        public function rechercherNbElevesParEcole($ecole_ID){
+		
+			TypeException::estInteger($ecole_ID);
+			
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(*) as NbEleves
+												FROM
+													utilisateurs u,
+													ecoles_par_utilisateur epu
+												WHERE
+													u.role = 1 AND
+													epu.ecole_ID = '".$ecole_ID."'
+												");
+            $aNombreEleves = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreEleves;
+			
         }
         
-        public function rechercherNbTutorielsParEcole(){
+        public function rechercherNbTutorielsParEcole($ecole_ID){
+		
+			TypeException::estInteger($ecole_ID);
+			
+			$oConnexion = new MySqliLib();
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(*) as NbTutoriels
+												FROM
+													contenu c
+												WHERE
+													ecole_ID = '".$ecole_ID."'
+												");
+			$aNombreTutoriels = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTutoriels;
         
         }
+		
+		
+		public function rechercherNbTutoratsParMatiere($tuteur_ID, $mois){
+		//rechercher nombre de tutoriels par matière soumis par un tuteur pour un mois donnéé
+		
+			TypeException::estInteger($tuteur_ID);
+			TypeException::estInteger($mois);
+			if($mois > 12 || $mois < 1){
+				throw new Exception("Exception::Le mois doit être un nombre valide");
+			}
+		
+			$annee_debut = "";
+			$annee_fin = "";
+			
+			if(date("n") <= 6){
+				$annee_debut = date("Y")-1;
+				$annee_fin = date("Y");
+			}
+			else{
+				$annee_debut = date("Y");
+				$annee_fin = date("Y")+1;
+			}
+			
+			
+			if($mois <=6){
+				$annee_en_cours = $annee_fin;
+			}else{
+					$annee_en_cours = $annee_debut;
+					}
+				$date = $annee_en_cours.'-'.$mois.'%';
+		
+			$oConnexion = new MySqliLib();
+			
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(*) as NbTutorielsMatiere, m.nom
+												FROM
+													contenu c, matieres m
+												WHERE
+													c.soumis_par = '".$tuteur_ID."' AND
+													c.date_soumis LIKE '".$date."' AND
+													c.matiere_ID = m.matiere_ID
+												AND
+													(
+														(YEAR(c.date_soumis) = '".$annee_debut."' AND MONTH(c.date_soumis) >= '8')
+														OR
+														(YEAR(c.date_soumis) = '".$annee_fin."' AND MONTH(c.date_soumis) <= '6')
+													)
+												GROUP BY
+													c.matiere_ID
+												ORDER BY m.nom ASC
+												");
+												
+			$aNombreTutorielsMatiere = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTutorielsMatiere;
+		}
+		
+		
+		public function rechercherNbTutoratsApprouvesParProf($prof_ID, $mois){
+		//rechercher nombre de tutoriels approuvés par matière pour un prof pour un mois donné
+		 
+			TypeException::estInteger($prof_ID);
+			TypeException::estInteger($mois);
+			if($mois > 12 || $mois < 1){
+				throw new Exception("Exception::Le mois doit être un nombre valide");
+			}
+
+			$annee_debut = "";
+			$annee_fin = "";
+			
+			if(date("n") <= 6){
+				$annee_debut = date("Y")-1;
+				$annee_fin = date("Y");
+			}
+			else{
+				$annee_debut = date("Y");
+				$annee_fin = date("Y")+1;
+			}
+			
+			
+			if($mois <=6){
+				$annee_en_cours = $annee_fin;
+			}else{
+					$annee_en_cours = $annee_debut;
+					}
+				$date = $annee_en_cours.'-'.$mois.'%';
+		
+			$oConnexion = new MySqliLib();
+			
+			$oResultat = $oConnexion->executer("SELECT
+													COUNT(*) as NbTutorielsApprouve, m.nom
+												FROM
+													contenu c, matieres m
+												WHERE
+													c.approuve_par = '".$prof_ID."' AND
+													c.date_approuve LIKE '".$date."' AND
+													c.matiere_ID = m.matiere_ID
+												AND
+													(
+														(YEAR(c.date_approuve) = '".$annee_debut."' AND MONTH(c.date_approuve) >= '8')
+														OR
+														(YEAR(c.date_approuve) = '".$annee_fin."' AND MONTH(c.date_approuve) <= '6')
+													)
+												GROUP BY
+													c.matiere_ID
+												ORDER BY m.nom ASC
+												");
+			$aNombreTutosMatiereApprouve = $oConnexion->recupererTableau($oResultat);
+			
+			return $aNombreTutosMatiereApprouve;
+			
+		}
+		
     }
 ?>
--- test/Statistique.test.php
+++ test/Statistique.test.php
@@ -1,4 +1,353 @@
-<div class="panel panel-primary col-xs-12">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+	<head>
+
+		<title>Test unitaire</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link href="../css/global.css" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+		<div id="header">
+			<h1>Test - Modèles Statistiques</h1>
+		</div>
+		<div id="contenu">
+			<?php 
+                // Placer vos tests unitaires ici...
+                /* La classe à tester */
+				require_once("../modeles/Statistique.class.php");
+				require_once("../lib/TypeException.class.php");
+            ?>
+			
+			<h2>rechercherNbVisitesParMois($user_id) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbVisitesParMois(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbVisitesParMois($user_id) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbVisitesParMois("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbTutoratsCrees($commmission_ID) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsCrees(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTutoratsCrees($commmission_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsCrees("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbTutoratsApprouves($commission_ID) => integer 30</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsApprouves(30);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTutoratsApprouves($commission_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsApprouves("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbTuteursConversation($iEleveId) => integer 1</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTuteursConversation(1);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTuteursConversation($iEleveId) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTuteursConversation("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbElevesConversation($iTuteurId) => integer 5</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbElevesConversation(5);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbElevesConversation($iTuteurId) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbElevesConversation("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbTuteursParEcole($ecole_ID) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTuteursParEcole(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTuteursParEcole($ecole_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTuteursParEcole("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbProfsParEcole($ecole_ID) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbProfsParEcole(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbProfsParEcole($ecole_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbProfsParEcole("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbElevesParEcole($ecole_ID) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbElevesParEcole(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbElevesParEcole($ecole_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbElevesParEcole("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				<br/>
+				<h2>rechercherNbTutorielsParEcole($ecole_ID) => integer 25</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutorielsParEcole(25);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTutorielsParEcole($ecole_ID) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutorielsParEcole("Silvia");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				<br/>
+				<h2>rechercherNbTutoratsParMatiere($tuteur_ID, $mois) => integer (25, 12)</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsParMatiere(25, 12);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				<h2>rechercherNbTutoratsParMatiere($tuteur_ID, $mois) => integer (25, 30)</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsParMatiere(25, 30);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTutoratsParMatiere($tuteur_ID, $mois) => chaine "Silvia"</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsParMatiere("Silvia", "12");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				<br/>
+				<h2>rechercherNbTutoratsApprouvesParProf($prof_ID, $mois) => integer (25, 2)</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsApprouvesParProf(25, 2);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				<h2>rechercherNbTutoratsApprouvesParProf($prof_ID, $mois) => integer (25, 30)</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsApprouvesParProf(25, 30);
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+				
+				
+			<h2>rechercherNbTutoratsApprouvesParProf($prof_ID, $mois) => chaine ("Silvia", "12")</h2>
+				<?php 
+				
+					try{
+						$oStatistique = new Statistique();
+						$oStatistique->rechercherNbTutoratsApprouvesParProf("Silvia", "12");
+						
+					}catch(Exception $e){
+						echo "<p>".$e->getMessage()."</p>";
+					}
+				
+				?>
+            
+            
+            <div class="panel panel-primary col-xs-12">
     <div class="panel-heading">
         Test de la vue afficheStatsPersonnellesEleve()
     </div>
@@ -26,3 +375,17 @@
         ?>
     </div>
 </div>
+		</div>
+		<div id="footer">
+
+		</div>
+	</body>
+</html>
+
+
+
+
+
+
+
+
