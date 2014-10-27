cdd8b5a425505578cd043c3c036d0e25ed66a10e
antoine818 <gouin.maisonneuve@hotmail.co.uk>
2014-10-27 19:24:48 -0400

    Intégration de la page d'erreur 404 custom

--- vues/Vue.class.php
+++ vues/Vue.class.php
@@ -18,9 +18,42 @@ class Vue {
     //Param 2 = Classe de couleur du message d'alert bootstrap (default, primary, warning, danger, succes, info, etc.)
     private $aMessage;
     
-    public function afficheErreur404(){
-        //Page qui dit "Soit vous vous êtes trompé de lien ou vous n'avez pas l'autorisation d'aller à cet endroit"
-        echo "erreur 404";
+    public function afficheErreur404(){?>
+        <div id="message">
+            <?php 
+                if($this->getMessage()){
+                    $aMessage = $this->getMessage();
+                    echo '<div class="alert alert-'.$aMessage[1].'">'.$aMessage[0].'</div>';
+                }
+            ?>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-sm-offset-0">
+                <div class="">
+
+                    <h1 class="text-center col-sm-12 text-danger page-header">
+                        <strong>Erreur 404</strong>
+                    </h1>
+
+                    <h2 class="col-sm-offset-0 col-sm-12 bold">
+                        <strong>Vous avez été redirigé ici pour l'une des raisons suivantes:</strong>
+                    </h2>
+                    <h3 class="col-sm-offset-0 col-sm-12 bold">
+                        <strong>Vous ne détenez pas les droits requis pour accéder au contenu de la page recherchée.</strong>
+                    </h3>
+                    <h3 class="col-sm-offset-0 col-sm-12 bold">
+                        <strong>La page que vous recherchez est en construction.</strong>
+                    </h3>
+                     <h3 class="col-sm-offset-0 col-sm-12 bold">
+                        <strong>La page que vous recherchez n'existe pas.</strong>
+                    </h3>
+                    <div class="col-sm-offset-0 col-sm-12 page-header">       
+                    </div>
+                        
+                </div>
+            </div>
+        </div>
+    <?php
     }
     
     public function setMessage($message){
